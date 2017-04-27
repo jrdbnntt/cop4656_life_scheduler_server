9878a43ab410a24e25252fc2d5f2ddcb2af1caaf
jrdbnntt <jrdbnntt@gmail.com>
2017-04-27 09:42:44 -0400

    api route /schedule/get/time_allotments

--- api/views/schedule/get/time_allotments.py
+++ api/views/schedule/get/time_allotments.py
@@ -4,14 +4,18 @@
 from django import forms
 from jrdbnntt_com.views.generic import ApiView
 from jrdbnntt_com.util import acl
+from jrdbnntt_com.util.forms import JsonField
+from api.models import TimeAllotment
+from django.utils import timezone
 
 
 class RequestForm(forms.Form):
-    pass
+    past = forms.BooleanField(required=False)
+    complete_tasks = forms.BooleanField(required=False)
 
 
 class ResponseForm(forms.Form):
-    pass
+    time_allotments = JsonField()
 
 
 class TimeAllotmentsView(ApiView):
@@ -20,4 +24,22 @@ class TimeAllotmentsView(ApiView):
     access_manager = acl.AccessManager(acl_accept=[acl.groups.USER])
 
     def work(self, request, req: dict, res: dict):
-        pass
+        qs = TimeAllotment.objects.filter(
+            task__parent_goal__user=request.user,
+            task__completed=req['complete_tasks'] is True,
+        )
+
+        if req['past']:
+            qs = qs.filter(end_time__lte=timezone.now())
+
+        time_allotments = []
+        for ta in qs.all():
+            time_allotments.append({
+                'task_id': ta.task_id,
+                'task_title': ta.task.title,
+                'start_time': ta.start_time.isoformat(),
+                'end_time': ta.end_time.isoformat(),
+                'duration': ta.duration_m()
+            })
+
+        res['time_allotments'] = time_allotments
