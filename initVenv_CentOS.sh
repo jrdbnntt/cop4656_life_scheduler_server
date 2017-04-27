e8018018eaa1c450d14b6febbb40fc711df7f37c
jrdbnntt <jrdbnntt@gmail.com>
2017-04-27 03:17:15 -0400

    centos psycopg2 fix script - added logs

--- initVenv_CentOS.sh
+++ initVenv_CentOS.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
 # Install fresh virtual environment
+echo -e "\n\nINSTALLING FRESH VIRTUAL ENVIRONMENT\n"
 rm -rf ./venv
 virtualenv -p /bin/python3.5 --no-site-packages venv
 
 # Install project requirements
+echo -e "\n\nINSTALLING REQUIREMENTS\n"
 ./venv/bin/pip3.5 install -r ./requirements.txt
 
 # Correct SELinux permissions
+echo -e "\n\nFIXING PERMISSIONS\n"
 function fixPermission () {
-    semanage fcontext -a -t $1 $2
+    semanage fcontext -a -v -t $1 $2
     restorecon -R -v $2
 }
 
@@ -18,3 +21,5 @@ CONTEXT=httpd_sys_script_exec_t
 find $(pwd)/venv/lib/python3.5/site-packages/psycopg2/ -iname '*.so*'| while read line; do
     fixPermission ${CONTEXT} ${line}
 done
+
+echo -e "\n\nCOMPLETE"
