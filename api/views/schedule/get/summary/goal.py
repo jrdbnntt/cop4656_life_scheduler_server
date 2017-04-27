85f2cc5f874d50ff4361802d07c68dab787b311c
jrdbnntt <jrdbnntt@gmail.com>
2017-04-27 08:32:09 -0400

    api route /schedule/get/summary/goal

--- api/views/schedule/get/summary/goal.py
+++ api/views/schedule/get/summary/goal.py
@@ -4,14 +4,20 @@
 from django import forms
 from jrdbnntt_com.views.generic import ApiView
 from jrdbnntt_com.util import acl
+from django.core.exceptions import ValidationError
+from api.models import Goal, Task
+from queue import Queue
 
 
 class RequestForm(forms.Form):
-    pass
+    goal_id = forms.IntegerField()
 
 
 class ResponseForm(forms.Form):
-    pass
+    title = forms.CharField()
+    completed = forms.BooleanField()
+    total_child_tasks = forms.IntegerField()
+    total_child_tasks_completed = forms.IntegerField()
 
 
 class GoalView(ApiView):
@@ -20,4 +26,38 @@ class GoalView(ApiView):
     access_manager = acl.AccessManager(acl_accept=[acl.groups.USER])
 
     def work(self, request, req: dict, res: dict):
-        pass
+        goal = Goal.objects.filter(id=req['goal_id'], user=request.user).all()
+        if len(goal) == 0:
+            raise ValidationError('Invalid goal_id')
+        goal = goal[0]
+
+        # Breadth-first sum of child goals' tasks
+        total_child_tasks = 0
+        total_child_tasks_completed = 0
+        goal_q = Queue()
+        goal_q.put(goal)
+
+        while not goal_q.empty():
+            curr_goal = goal_q.get()
+
+            # Add counts for this goal
+            tasks = Task.objects.filter(parent_goal=curr_goal)
+            total_child_tasks += tasks.count()
+            total_child_tasks_completed += tasks.filter(completed=True).count()
+
+            # Add any sub goals to queue
+            for new_goal in Goal.objects.filter(parent_goal=curr_goal).all():
+                goal_q.put(new_goal)
+
+        # Make sure completed is accurate
+        completed = total_child_tasks / total_child_tasks_completed == 1
+        if goal.completed is not completed:
+            goal.completed = completed
+            goal.save()
+
+        # Set response
+        res['title'] = goal.title
+        res['completed'] = completed
+        res['total_child_tasks'] = total_child_tasks
+        res['total_child_tasks_completed'] = total_child_tasks_completed
+
