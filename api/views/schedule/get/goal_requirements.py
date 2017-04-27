f6b52972761d5db48f86c361913d74840657c555
jrdbnntt <jrdbnntt@gmail.com>
2017-04-27 07:53:37 -0400

    api route schedule/get/goal_requirements

--- api/views/schedule/get/goal_requirements.py
+++ api/views/schedule/get/goal_requirements.py
@@ -5,14 +5,18 @@
 from django import forms
 from jrdbnntt_com.views.generic import ApiView
 from jrdbnntt_com.util import acl
+from jrdbnntt_com.util.forms import JsonField
+from api.models import Goal, Task
+from django.core.exceptions import ValidationError
 
 
 class RequestForm(forms.Form):
-    pass
+    parent_goal_id = forms.IntegerField(required=False)
 
 
 class ResponseForm(forms.Form):
-    pass
+    goal_ids = JsonField()
+    task_ids = JsonField()
 
 
 class GoalRequirementsView(ApiView):
@@ -21,4 +25,20 @@ class GoalRequirementsView(ApiView):
     access_manager = acl.AccessManager(acl_accept=[acl.groups.USER])
 
     def work(self, request, req: dict, res: dict):
-        pass
+        task_ids = []
+
+        if req['parent_goal_id'] is None:
+            # Return top-level goals
+            goal_ids = Goal.objects.filter(user=request.user, parent_goal__isnull=True).values_list('id', flat=True)
+        else:
+            # Return goals & tasks from parent goal
+            parent_goal = Goal.objects.filter(id=req['parent_goal_id'], user=request.user).all()
+            if len(parent_goal) == 0:
+                raise ValidationError('Invalid parent_goal_id')
+            parent_goal = parent_goal[0]
+
+            goal_ids = Goal.objects.filter(parent_goal=parent_goal).values_list('id', flat=True)
+            task_ids = Task.objects.filter(parent_goal=parent_goal).values_list('id', flat=True)
+
+        res['goal_ids'] = goal_ids
+        res['task_ids'] = task_ids
